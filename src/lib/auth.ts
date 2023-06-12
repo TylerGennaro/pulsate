bf983cc9e39a5fd6e131fcb8a4a1b87f11696ce7
Tyler Gennaro <tylergennaro10@gmail.com>
2023-06-12 15:11:41 -0400

    removed azure provider for testing

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,6 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { NextAuthOptions } from 'next-auth';
 import { db } from './prisma';
 import GoogleProvider from 'next-auth/providers/google';
-import AzureADProvider from 'next-auth/providers/azure-ad';
 
 function getGoogleCredentials(): { clientId: string; clientSecret: string } {
 	const clientId = process.env.GOOGLE_CLIENT_ID;
@@ -13,67 +12,15 @@ function getGoogleCredentials(): { clientId: string; clientSecret: string } {
 	return { clientId, clientSecret };
 }
 
-function getAzureCredentials() {
-	const clientId = process.env.AZURE_AD_CLIENT_ID;
-	const clientSecret = process.env.AZURE_AD_CLIENT_SECRET;
-	const tenantId = process.env.AZURE_AD_TENANT_ID;
-	if (!clientId || !clientSecret || !tenantId) {
-		throw new Error('Missing Azure AD Client ID, Client Secret, or Tenant ID');
-	}
-	return { clientId, clientSecret, tenantId };
-}
-
 export const authOptions: NextAuthOptions = {
 	adapter: PrismaAdapter(db),
 	session: {
 		strategy: 'jwt',
-	},
-	pages: {
-		signIn: '/signin',
 	},
 	providers: [
 		GoogleProvider({
 			clientId: getGoogleCredentials().clientId,
 			clientSecret: getGoogleCredentials().clientSecret,
 		}),
-		AzureADProvider({
-			clientId: getAzureCredentials().clientId,
-			clientSecret: getAzureCredentials().clientSecret,
-			tenantId: getAzureCredentials().tenantId,
-		}),
 	],
-	callbacks: {
-		async session({ token, session }) {
-			if (token && session?.user) {
-				session.user.id = token.id;
-				session.user.name = token.name;
-				session.user.email = token.email;
-				session.user.image = token.picture;
-			}
-
-			return session;
-		},
-		async jwt({ token, user }) {
-			const dbUser = await db.user.findFirst({
-				where: {
-					email: token.email,
-				},
-			});
-
-			if (!dbUser) {
-				token.id = user!.id;
-				return token;
-			}
-
-			return {
-				id: dbUser.id,
-				name: dbUser.name,
-				email: dbUser.email,
-				picture: dbUser.image,
-			};
-		},
-		redirect({ url, baseUrl }) {
-			return url.startsWith(baseUrl) ? url : baseUrl;
-		},
-	},
 };
