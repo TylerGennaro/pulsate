133ad1d42bb062bdd5ba8e673d5a49603dee8442
Tyler Gennaro <tylergennaro10@gmail.com>
2023-06-13 11:52:51 -0400

    add signin to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from '@components/SignButton';
 import { ChevronRight } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -12,6 +13,7 @@ export default function Home() {
 				Inventory
 				<ChevronRight className='w-4 h-4 ml-2' />
 			</Link>
+			<SignInButton />
 			<div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
 				<p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
 					Get started by editing&nbsp;
