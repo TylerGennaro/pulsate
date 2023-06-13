e7fd18cd1f3ae7cfd8aa1b815a96567194257515
Tyler Gennaro <tylergennaro10@gmail.com>
2023-06-13 18:10:03 -0400

    Improved landing page responsivity

---
+++ src/app/LandingHeader.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import { SignInButton } from '@components/SignButton';
+import { ThemeToggle } from '@components/ThemeToggle';
+import { Button } from '@components/ui/button';
+import { ArrowRight, Menu, X } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
+
+export default function LandingHeader() {
+	const { data: session } = useSession();
+	const [sidebarOpen, setSidebarOpen] = useState(false);
+	const sidebarRef = useRef<HTMLDivElement>(null);
+	useEffect(() => {
+		window.addEventListener('resize', () => {
+			if (sidebarOpen) setSidebarOpen(false);
+		});
+		window.addEventListener('click', (e) => {
+			const target = e.target as Element;
+			if (
+				sidebarOpen &&
+				!sidebarRef.current?.contains(e.target as Node) &&
+				(typeof target.className !== 'string' ||
+					!target.className.includes('sidebar-toggle'))
+			)
+				setSidebarOpen(false);
+		});
+	}, [sidebarOpen]);
+	return (
+		<div
+			className='relative grid justify-between w-full grid-cols-2 p-6 border-r lg:grid-cols-3 sm:border-none'
+			ref={sidebarRef}
+		>
+			<Button
+				variant='ghost'
+				className='sm:hidden justify-self-start sidebar-toggle'
+				onClick={() => setSidebarOpen(!sidebarOpen)}
+			>
+				<Menu />
+			</Button>
+			<Image
+				src='/logo.svg'
+				alt='logo'
+				width={64}
+				height={64}
+				className='hidden lg:block'
+			/>
+			<div
+				className={`absolute top-0 left-0 flex flex-col h-screen gap-4 p-8 pr-16 font-semibold ${
+					sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+				} justify-center transition-transform shadow-md sm:translate-x-0 bg-foreground sm:shadow-none sm:items-center sm:p-0 sm:flex-row sm:h-auto justify-left sm:gap-12 sm:static`}
+			>
+				<Button
+					variant='ghost'
+					className='absolute p-1 top-2 right-2 sm:hidden'
+					onClick={() => setSidebarOpen(false)}
+				>
+					<X />
+				</Button>
+				<div className='relative sm:hidden'>
+					<Image src='/logo.svg' alt='logo' width={64} height={64} />
+					<hr className='my-4' />
+				</div>
+				<Link href='#'>Features</Link>
+				<Link href='#'>Pricing</Link>
+				<Link href='#'>Testimonials</Link>
+			</div>
+			<div className='flex items-center justify-end gap-2'>
+				<ThemeToggle />
+				{session ? (
+					<Link href='/inventory'>
+						<Button variant='outline'>Dashboard</Button>
+					</Link>
+				) : (
+					<SignInButton variant='outline'>
+						Sign In
+						<ArrowRight className='w-4 h-4 ml-2' />
+					</SignInButton>
+				)}
+			</div>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,21 @@
-import { SignInButton } from '@components/SignButton';
 import { Button } from '@components/ui/button';
-import { authOptions } from '@lib/auth';
-import { ArrowRight } from 'lucide-react';
-import { getServerSession } from 'next-auth';
-import Image from 'next/image';
 import Link from 'next/link';
+import LandingHeader from './LandingHeader';
 
 export default async function Home() {
-	const session = await getServerSession(authOptions);
 	return (
-		<main className='min-h-screen flex flex-col items-center'>
-			<div className='w-full flex justify-between items-center p-6'>
-				<Image src='/logo.svg' alt='logo' width={64} height={64} />
-				<div className='flex gap-12 font-semibold'>
-					<Link href='#'>Features</Link>
-					<Link href='#'>Pricing</Link>
-					<Link href='#'>Testimonials</Link>
-				</div>
-				{session ? (
-					<Link href='/inventory'>
-						<Button variant='outline'>Dashboard</Button>
-					</Link>
-				) : (
-					<SignInButton variant='outline'>
-						Sign In
-						<ArrowRight className='w-4 h-4 ml-2' />
-					</SignInButton>
-				)}
-			</div>
-			<div className='grid place-content-center grow max-w-screen-sm text-center'>
+		<main className='flex flex-col items-center min-h-screen bg-foreground'>
+			<LandingHeader />
+			<div className='grid max-w-screen-sm p-4 text-center place-content-center grow'>
 				<h1 className='text-5xl font-bold'>
 					The solution to organizing medical closets
 				</h1>
-				<h3 className='mt-4 text-muted-text text-xl'>
+				<h3 className='mt-4 text-xl text-muted-text'>
 					Effortlessly organize your medical closet and manage inventory with
 					ease using this intuitive solution.
 				</h3>
 				<Link href='/inventory'>
-					<Button className='mt-8 w-fit mx-auto'>Get started</Button>
+					<Button className='mx-auto mt-8 shadow-md w-fit'>Get started</Button>
 				</Link>
 			</div>
 		</main>
