d6937108610756ac0291938836e467d1f7112568
Tyler Gennaro <tylergennaro10@gmail.com>
2023-06-20 00:30:34 -0400

    Fixed metadata, put Exec in the middle

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -4,6 +4,13 @@ import { Button } from '@components/ui/button';
 import { ArrowRight } from 'lucide-react';
 import FAQ from './FAQ';
 import CompareTable from './CompareTable';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Plans and Pricing | Pulsate',
+	description:
+		"Choose the best plan for your orgnization and don't overpay. Plans are differentiated by scale and features.",
+};
 
 const plans = {
 	Free: {
@@ -19,19 +26,6 @@ const plans = {
 			'Location sharing': false,
 		},
 	},
-	Manager: {
-		price: 299,
-		monthly: 15,
-		perks: {
-			'Inventory management': true,
-			'Expiration date tracking': true,
-			'Multiple locations': true,
-			'Activity log': true,
-			'Printable QR Codes': true,
-			'Inventory notifications': true,
-			'Location sharing': false,
-		},
-	},
 	Executive: {
 		price: 299,
 		monthly: 40,
@@ -44,6 +38,19 @@ const plans = {
 			'Printable QR Codes': true,
 			'Inventory notifications': true,
 			'Location sharing': true,
+		},
+	},
+	Manager: {
+		price: 299,
+		monthly: 15,
+		perks: {
+			'Inventory management': true,
+			'Expiration date tracking': true,
+			'Multiple locations': true,
+			'Activity log': true,
+			'Printable QR Codes': true,
+			'Inventory notifications': true,
+			'Location sharing': false,
 		},
 	},
 };
@@ -69,7 +76,7 @@ export default function Page() {
 					</div>
 				</div>
 			</div>
-			<div className='flex flex-wrap justify-center gap-8'>
+			<div className='flex flex-wrap justify-center gap-2'>
 				{Object.entries(plans).map(([name, { monthly, price, perks }]) => (
 					<PlanCard
 						key={name}
