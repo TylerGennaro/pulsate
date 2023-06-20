f867eca7523d18d85e23ed0a03df00f389559c90
Tyler Gennaro <tylergennaro10@gmail.com>
2023-06-20 13:50:46 -0400

    Adjusted header

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -5,6 +5,9 @@ import { ArrowRight } from 'lucide-react';
 import FAQ from './FAQ';
 import CompareTable from './CompareTable';
 import { Metadata } from 'next';
+import NavHeader from '@components/NavHeader';
+import Link from 'next/link';
+import Plans from './Plans';
 
 export const metadata: Metadata = {
 	title: 'Plans and Pricing | Pulsate',
@@ -12,87 +15,41 @@ export const metadata: Metadata = {
 		"Choose the best plan for your orgnization and don't overpay. Plans are differentiated by scale and features.",
 };
 
-const plans = {
-	Free: {
-		price: 0,
-		monthly: 0,
-		perks: {
-			'Inventory management': true,
-			'Expiration date tracking': true,
-			'Multiple locations': false,
-			'Activity log': false,
-			'Printable QR Codes': false,
-			'Inventory notifications': false,
-			'Location sharing': false,
-		},
-	},
-	Executive: {
-		price: 299,
-		monthly: 40,
-		best: true,
-		perks: {
-			'Inventory management': true,
-			'Expiration date tracking': true,
-			'Multiple locations': true,
-			'Activity log': true,
-			'Printable QR Codes': true,
-			'Inventory notifications': true,
-			'Location sharing': true,
-		},
-	},
-	Manager: {
-		price: 299,
-		monthly: 15,
-		perks: {
-			'Inventory management': true,
-			'Expiration date tracking': true,
-			'Multiple locations': true,
-			'Activity log': true,
-			'Printable QR Codes': true,
-			'Inventory notifications': true,
-			'Location sharing': false,
-		},
-	},
-};
-
 export default function Page() {
 	return (
-		<div className='h-[100dvh] flex flex-col gap-32 p-16 overflow-auto scroll-smooth'>
-			<div className='flex justify-center w-full'>
-				<div className='flex flex-col items-center max-w-screen-md gap-2'>
-					<Header size='xl' className='text-center'>
-						Plans suited for your needs
-					</Header>
-					<span className='w-3/4 text-center text-muted'>
-						Choose the best plan for your orgnization and don&apos;t overpay.
-						Plans are differentiated by scale and features.
-					</span>
-					<div className='flex gap-2 mt-2'>
-						<a href='#faq'>
-							<Button variant='outline'>FAQ</Button>
-						</a>
-						<a href='#compare'>
-							<Button>
-								Compare Plans
-								<ArrowRight className='w-4 h-4 ml-2' />
-							</Button>
-						</a>
+		<main className='flex flex-col scroll-smooth'>
+			<NavHeader>
+				<Link href='/'>Home</Link>
+				<a href='#compare'>Plans</a>
+				<a href='#faq'>FAQ</a>
+			</NavHeader>
+			<div className='flex flex-col items-center gap-32 p-16'>
+				<div className='flex justify-center w-full'>
+					<div className='flex flex-col items-center max-w-screen-md gap-2'>
+						<Header size='xl' className='text-center'>
+							Plans suited for your needs
+						</Header>
+						<span className='w-3/4 text-center text-muted'>
+							Choose the best plan for your orgnization and don&apos;t overpay.
+							Plans are differentiated by scale and features.
+						</span>
+						<div className='flex gap-2 mt-2'>
+							<a href='#faq'>
+								<Button variant='outline'>FAQ</Button>
+							</a>
+							<a href='#compare'>
+								<Button>
+									Compare Plans
+									<ArrowRight className='w-4 h-4 ml-2' />
+								</Button>
+							</a>
+						</div>
 					</div>
 				</div>
+				<Plans />
+				<CompareTable />
+				<FAQ />
 			</div>
-			<div className='flex flex-wrap justify-center gap-2'>
-				{Object.entries(plans).map(([name, { monthly, price, perks }]) => (
-					<PlanCard
-						key={name}
-						name={name}
-						monthly={monthly}
-						price={price}
-						perks={perks}
-					/>
-				))}
-			</div>
-			<CompareTable />
-			<FAQ />
-		</div>
+		</main>
 	);
 }
