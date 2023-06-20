c254aac8633ee264ef5a671bfb79aeedf671b5c8
Tyler Gennaro <tylergennaro10@gmail.com>
2023-06-20 00:32:11 -0400

    Commented unfinished code

--- src/app/api/purchase/route.ts
+++ src/app/api/purchase/route.ts
@@ -1,11 +1,11 @@
-import { Stripe } from "@stripe/stripe-js";
+// import { Stripe } from "@stripe/stripe-js";
 
-const params: Stripe.Checkout.SessionCreateParams = {
-	payment_method_types: ["card"],
-	submit_type: "pay",
-	line_items: [
-		{
-			name: "Manager Package",
-			amount: formatAmountForStripe(399, CURRENCY),
-		}
-	]
+// const params: Stripe.Checkout.SessionCreateParams = {
+// 	payment_method_types: ["card"],
+// 	submit_type: "pay",
+// 	line_items: [
+// 		{
+// 			name: "Manager Package",
+// 			amount: formatAmountForStripe(399, CURRENCY),
+// 		}
+// 	]
