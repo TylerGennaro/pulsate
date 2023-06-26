f51447da060225211fe6a941ec927ccaa4b37b67
Tyler Gennaro <tylergennaro10@gmail.com>
2023-06-26 01:59:02 -0400

    Added dot grid and background blue spotlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Header from '@components/ui/header';
 import Features from './Features';
 import HeroHeading from '@components/HeroHeading';
 import { ArrowRight } from 'lucide-react';
+import { Patterns } from '@components/BackgroundPattern';
 
 export default async function Home() {
 	return (
@@ -15,17 +16,19 @@ export default async function Home() {
 				<Link href='#pricing'>Pricing</Link>
 			</NavHeader>
 			<div className='flex flex-col items-center pb-16'>
-				<div className='flex flex-col items-center justify-center h-[100vh] max-w-screen-md gap-2 px-8'>
-					<Header size='xl' className='text-center'>
-						The solution to organizing medical closets
-					</Header>
-					<span className='w-3/4 text-lg text-center text-muted'>
-						Effortlessly organize your medical closet and manage inventory with
-						ease using this intuitive solution.
-					</span>
-					<Link href='/app' className='mt-2 w-fit'>
-						<Button className='shadow-md'>Get started</Button>
-					</Link>
+				<div className='h-[100vh] max-w-screen-md px-8'>
+					<div className='absolute absolute-center bg-[radial-gradient(#1d4ed840,_transparent_70%)] h-2/3 aspect-square -z-20' />
+					<Patterns.DotGrid className='absolute absolute-center -z-10 h-3/4 aspect-square' />
+					<HeroHeading
+						title='The solution to organizing medical closets'
+						description='Effortlessly organize your medical closet and manage inventory with
+						ease using this intuitive solution.'
+						className='mt-32'
+					>
+						<Link href='/app' className='mt-2 w-fit'>
+							<Button className='shadow-md'>Get started</Button>
+						</Link>
+					</HeroHeading>
 				</div>
 				<Features />
 				<div id='pricing' className='flex flex-col items-center px-8 py-16'>
