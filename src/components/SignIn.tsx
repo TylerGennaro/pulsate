3cefb30742121b4d60b818f7ed009d44098df262
Tyler Gennaro <tylergennaro10@gmail.com>
2023-06-26 01:58:00 -0400

    Fixed logo on sign in and added theme toggle

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -6,6 +6,9 @@ import { FaGoogle, FaMicrosoft } from 'react-icons/fa';
 import { IconType } from 'react-icons';
 import Heading from './ui/heading';
 import Image from 'next/image';
+import Logo from './Logo';
+import { useSearchParams } from 'next/navigation';
+import { ThemeToggle } from './ThemeToggle';
 
 interface ProviderProps {
 	name: string;
@@ -37,13 +40,15 @@ function Provider({
 	);
 }
 
-export default function SignIn({ callbackUrl }: { callbackUrl?: string }) {
+export default function SignIn() {
+	const searchParams = useSearchParams();
+	const callbackUrl = searchParams.get('callbackUrl') ?? undefined;
 	return (
 		<div className='grid w-screen h-screen place-items-center'>
-			<div className='flex flex-col w-full max-w-sm gap-4 p-8 rounded-md shadow-md bg-zinc-50 dark:bg-zinc-900'>
-				<div className='flex items-center mb-4'>
-					<Image src='/logo.svg' alt='logo' width={64} height={64} />
-					<span className='text-2xl font-semibold'>Pulsate</span>
+			<div className='flex flex-col w-full max-w-sm gap-4 p-8 border rounded-md shadow-md bg-zinc-50 dark:bg-zinc-900'>
+				<div className='flex items-center justify-between mb-4'>
+					<Logo />
+					<ThemeToggle />
 				</div>
 				<Heading
 					header='Sign In'
