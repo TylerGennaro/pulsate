0365a2a8b3a80bd30752d0404da967d308f3b791
Tyler Gennaro <tylergennaro10@gmail.com>
2023-07-01 16:30:19 -0400

    Fixed build error

--- src/app/app/settings/columns.ts
+++ src/app/app/settings/columns.ts
@@ -1,3 +1,4 @@
+import { Payment } from '@prisma/client';
 import { ColumnDef } from '@tanstack/react-table';
 
-export const columns: ColumnDef[] = [];
+export const columns: ColumnDef<Payment>[] = [];
