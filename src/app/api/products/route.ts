f3108b7f16948c7c5a0041e23a56d46f0d158aec
Tyler Gennaro <tylergennaro10@gmail.com>
2023-07-24 14:37:27 -0400

    Adjusted zod parsing rules

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -4,7 +4,6 @@ import { db } from '@lib/prisma';
 import { catchError } from '@lib/utils';
 import { LogType } from '@prisma/client';
 import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
 import { NextResponse } from 'next/server';
 import { z } from 'zod';
 
@@ -24,14 +23,17 @@ const schema = z.object({
 	max: z.coerce
 		.number({ invalid_type_error: 'Max quantity is not a number.' })
 		.int()
-		.min(0, { message: 'Maximum quantity must be greater than 0.' }),
+		.min(0, { message: 'Maximum quantity must be greater than 0.' })
+		.nullable(),
 	packageType: z.enum(['single', 'pack', 'box', 'case'], {
 		invalid_type_error: 'Package type is not valid.',
 		required_error: 'Package type is required.',
 	}),
 	position: z
 		.string()
-		.max(50, { message: 'Name must be less than 50 characters.' }),
+		.max(50, { message: 'Name must be less than 50 characters.' })
+		.nullable(),
+	url: z.string().url({ message: 'URL is not valid.' }).nullable(),
 });
 
 export async function POST(req: Request) {
@@ -44,16 +46,19 @@ export async function POST(req: Request) {
 		return new NextResponse('Invalid location ID.', { status: 400 });
 
 	try {
-		const { name, min, max, packageType, position } = schema.parse(data);
+		const { name, min, max, packageType, position, url } = schema.parse(data);
+
+		// const tinyUrl = url ? await shortenURL(url) : null;
 
 		const newProduct = await db.product.create({
 			data: {
 				name,
 				min,
-				max,
+				max: max ?? undefined,
 				package: packageType,
 				position,
 				locationId: data.locationId,
+				url,
 			},
 		});
 		log(LogType.PRODUCT_ADD, {
@@ -63,6 +68,7 @@ export async function POST(req: Request) {
 			status: 200,
 		});
 	} catch (e) {
+		console.log(e);
 		return catchError(e);
 	}
 }
@@ -77,7 +83,7 @@ export async function PUT(req: Request) {
 	const id = searchParams.get('id') || '';
 
 	try {
-		const { name, min, max, packageType, position } = schema.parse(data);
+		const { name, min, max, packageType, position, url } = schema.parse(data);
 		const product = await db.product.findFirst({
 			where: {
 				id,
@@ -85,6 +91,32 @@ export async function PUT(req: Request) {
 		});
 		if (!product)
 			return new NextResponse('Could not find product.', { status: 404 });
+
+		// let shortUrl: number | null = null;
+		// if (url) {
+		// 	if (product.url) {
+		// 		const matchingURL = await db.product.findMany({
+		// 			where: {
+		// 				url: product.url,
+		// 			},
+		// 		});
+		// 		if (matchingURL.length > 1) {
+		// 			const newUrl = await shortenURL(url);
+		// 			shortUrl = newUrl;
+		// 		} else {
+		// 			console.log('updating to ' + url);
+		// 			console.log(await updateShortURL(product.url, url));
+		// 			shortUrl = product.url;
+		// 		}
+		// 	} else {
+		// 		shortUrl = await shortenURL(url);
+		// 	}
+		// } else {
+		// 	if (product.url) {
+		// 		await deleteShortUrl(product.url);
+		// 	}
+		// }
+
 		const updatedProduct = await db.product.update({
 			where: {
 				id,
@@ -92,9 +124,10 @@ export async function PUT(req: Request) {
 			data: {
 				name,
 				min,
-				max,
+				max: max ?? undefined,
 				package: packageType,
 				position,
+				url,
 			},
 		});
 		log(LogType.PRODUCT_UPDATE, {
@@ -113,7 +146,11 @@ export async function PUT(req: Request) {
 					? [`Package: ${product.package} → ${updatedProduct.package}`]
 					: []),
 				...(product.position !== updatedProduct.position
-					? [`Position: ${product.position} → ${updatedProduct.position}`]
+					? [
+							`Position: ${product.position || 'None'} → ${
+								updatedProduct.position || 'None'
+							}`,
+					  ]
 					: []),
 			].join(', '),
 		});
@@ -143,6 +180,11 @@ export async function DELETE(req: Request) {
 		});
 		if (!product)
 			return new NextResponse('Could not find product.', { status: 404 });
+
+		// if (product.url) {
+		// 	await deleteShortUrl(product.url);
+		// }
+
 		const deletedProduct = await db.product.delete({
 			where: {
 				id,
