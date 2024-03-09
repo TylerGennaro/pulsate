507c4e2e80b5650d4330be16ad1bbf9817e0cf87
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-08 20:47:07 -0500

    change manager user to location owner instead of the current user

--- src/app/app/[location]/page.tsx
+++ src/app/app/[location]/page.tsx
@@ -1,18 +1,16 @@
-import { Button } from '@components/ui/button';
-import { ScrollText } from 'lucide-react';
-import Link from 'next/link';
-import { db } from '@lib/prisma';
-import { Suspense } from 'react';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@lib/auth';
-import SignIn from '@components/SignIn';
-import { notFound } from 'next/navigation';
 import Container from '@components/Container';
-import { populateMetadata } from '@lib/utils';
-import NewProduct from '@components/location/NewProduct';
+import SignIn from '@components/SignIn';
 import EditLocation from '@components/location/EditLocation';
-import { fetchLocationInfo } from '@lib/data';
 import InventoryTable from '@components/location/InventoryTable';
+import NewProduct from '@components/location/NewProduct';
+import { Button } from '@components/ui/button';
+import { authOptions } from '@lib/auth';
+import { fetchLocationInfo } from '@lib/data';
+import { populateMetadata } from '@lib/utils';
+import { ScrollText } from 'lucide-react';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
 export async function generateMetadata({
 	params,
@@ -30,14 +28,14 @@ export default async function Inventory({
 }) {
 	const session = await getServerSession(authOptions);
 	if (!session) return <SignIn />;
-	const { name, userId } = await fetchLocationInfo(params.location);
+	const { name, userId, userName } = await fetchLocationInfo(params.location);
 	if (!name) return notFound();
 	if (userId !== session.user.id) return notFound();
 
 	return (
 		<Container
 			header={name}
-			description={`Managed by ${session.user.name}`}
+			description={`Managed by ${userName ?? 'Unknown User'}`}
 			action={<EditLocation name={name} id={params.location} />}
 			divider
 		>
