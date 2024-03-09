77cc44fea47f02c3210e922cba706f42f5dddb11
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-08 20:45:28 -0500

    add top margin to footer

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -75,7 +75,7 @@ const AlertDialogFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
 	<div
 		className={cn(
-			'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+			'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 mt-4',
 			className
 		)}
 		{...props}
