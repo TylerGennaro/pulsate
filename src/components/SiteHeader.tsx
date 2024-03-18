1b42fb9bc311e084640ee3c959b522d1d68c1519
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-18 00:15:11 -0400

    changed SignOut button to Dashboard

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -7,63 +7,70 @@ import { Button } from './ui/button';
 import { cn } from '@lib/utils';
 import { useSession } from 'next-auth/react';
 import { Skeleton } from './ui/skeleton';
-import { SignInButton, SignOutButton } from './SignButton';
+import { SignInButton } from './SignButton';
+import Link from 'next/link';
 
 export default function SiteHeader({
 	sidebarToggle,
 	sidebarOpen,
 	children,
 	className,
-	showSignIn = false,
+	showDashboard = false,
 }: {
 	sidebarToggle?: (open: boolean) => void;
 	sidebarOpen?: boolean;
 	children?: React.ReactNode;
 	className?: string;
-	showSignIn?: boolean;
+	showDashboard?: boolean;
 }) {
 	const { status } = useSession();
 	return (
 		<div
 			className={cn(
-				`w-full sticky top-0 h-16 bg-content border-b shadow-md flex items-center justify-between ${
-					sidebarToggle && 'lg:justify-end'
-				} px-4 z-30`,
+				'w-full sticky top-0 h-16 bg-content border-b shadow-md z-30',
 				className
 			)}
 		>
-			{sidebarToggle && (
-				<Button
-					variant='ghost'
-					size='sm'
-					className='lg:hidden sidebar-toggle'
-					onClick={() => sidebarToggle(!sidebarOpen)}
-				>
-					<Menu className='pointer-events-none' />
-					<span className='sr-only'>Toggle sidebar</span>
-				</Button>
-			)}
-			{children !== undefined && (
-				<div className='flex gap-4 font-semibold justify-self-start text-muted-foreground'>
-					{children}
-				</div>
-			)}
-			<div className='flex items-center gap-2'>
-				{showSignIn ? (
-					<>
-						{status === 'authenticated' && (
-							<>
-								<SignOutButton />
-								<Notifications />
-							</>
-						)}
-						{status === 'unauthenticated' && <SignInButton />}
-						{status === 'loading' && <Skeleton className='w-32 h-10' />}
-					</>
-				) : (
-					<Notifications />
+			<div
+				className={`h-full flex items-center justify-between px-4 max-w-screen-2xl mx-auto ${
+					sidebarToggle && 'lg:justify-end'
+				}`}
+			>
+				{sidebarToggle && (
+					<Button
+						variant='ghost'
+						size='sm'
+						className='lg:hidden sidebar-toggle'
+						onClick={() => sidebarToggle(!sidebarOpen)}
+					>
+						<Menu className='pointer-events-none' />
+						<span className='sr-only'>Toggle sidebar</span>
+					</Button>
 				)}
-				<ThemeToggle />
+				{children !== undefined && (
+					<div className='flex gap-4 font-semibold justify-self-start text-muted-foreground'>
+						{children}
+					</div>
+				)}
+				<div className='flex items-center gap-2'>
+					{showDashboard ? (
+						<>
+							{status === 'authenticated' && (
+								<>
+									<Link href='/app'>
+										<Button variant='outline'>Dashboard</Button>
+									</Link>
+									<Notifications />
+								</>
+							)}
+							{status === 'unauthenticated' && <SignInButton />}
+							{status === 'loading' && <Skeleton className='w-32 h-10' />}
+						</>
+					) : (
+						<Notifications />
+					)}
+					<ThemeToggle />
+				</div>
 			</div>
 		</div>
 	);
