f8b67d283da2edfadd2b3e6cb2f937a3fa4bd448
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-18 00:12:12 -0400

    adjusted checkout page layout

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -1,17 +1,18 @@
+import Logo from '@components/Logo';
 import SiteHeader from '@components/SiteHeader';
+import { Home } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className='h-screen flex flex-col'>
+		<div className='flex flex-col h-screen'>
 			<SiteHeader
-				showSignIn
+				showDashboard
 				className='absolute top-0 left-0 right-0 shadow-sm'
 			>
-				<Link href='/'>Home</Link>
-				<Link href='/app'>Inventory</Link>
+				<Logo />
 			</SiteHeader>
-			<div className='mt-16 flex justify-center h-full overflow-auto'>
+			<div className='flex justify-center h-full mt-16 overflow-auto'>
 				{children}
 			</div>
 		</div>
