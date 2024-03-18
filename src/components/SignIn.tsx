7e99002dfd088f1ebeb1a30d71f2e8a079deff71
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-18 00:14:58 -0400

    fixed colors and added dot grid bg

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -3,12 +3,11 @@
 import { signIn } from 'next-auth/react';
 import { FaGoogle, FaMicrosoft } from 'react-icons/fa';
 
-import { IconType } from 'react-icons';
-import Heading from './ui/heading';
-import Image from 'next/image';
-import Logo from './Logo';
 import { useSearchParams } from 'next/navigation';
+import { IconType } from 'react-icons';
+import Logo from './Logo';
 import { ThemeToggle } from './ThemeToggle';
+import Heading from './ui/heading';
 
 interface ProviderProps {
 	name: string;
@@ -45,7 +44,8 @@ export default function SignIn() {
 	const callbackUrl = searchParams.get('callbackUrl') ?? undefined;
 	return (
 		<div className='grid w-screen h-screen place-items-center'>
-			<div className='flex flex-col w-full max-w-sm gap-4 p-8 border rounded-md shadow-md bg-zinc-50 dark:bg-zinc-900'>
+			<div className="absolute top-0 left-0 w-screen h-screen bg-[url('/dot_grid.svg')] -z-[1]" />
+			<div className='flex flex-col w-full max-w-sm gap-4 p-8 border rounded-md shadow-md bg-content'>
 				<div className='flex items-center justify-between mb-4'>
 					<Logo />
 					<ThemeToggle />
