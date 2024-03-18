b8aedac713c5b6fd07a35bbc10a5e371a6157d1d
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-18 00:09:19 -0400

    added glow and recolored

--- src/app/Features.tsx
+++ src/app/Features.tsx
@@ -1,5 +1,4 @@
 import HeroHeading from '@components/HeroHeading';
-import Header from '@components/ui/header';
 import { DollarSign, Pointer, UploadCloud, UserCog } from 'lucide-react';
 
 const features = [
@@ -31,29 +30,29 @@ const features = [
 
 export default function Features() {
 	return (
-		<div
-			id='features'
-			className='flex flex-col items-center w-full gap-16 px-8 py-16 bg-content border-y'
-		>
-			<HeroHeading
-				title='We have what you need'
-				description='We develop the service with you in mind. Features are maintained to
+		<div id='features' className='w-full bg-content border-y'>
+			<div className='container relative flex flex-col items-center w-full gap-16 px-8 py-16 overflow-hidden'>
+				<div className='absolute top-0 w-2/3 h-40 -translate-x-1/2 -translate-y-1/2 bg-primary/10 dark:bg-white/10 left-1/2 rounded-[50%] blur-[60px]' />
+				<HeroHeading
+					title='We have what you need'
+					description='We develop the service with you in mind. Features are maintained to
 					ensure you spend less time worrying and more time focusing on what matters.'
-			/>
-			<div className='grid max-w-screen-md grid-cols-1 gap-16 sm:grid-cols-2'>
-				{features.map((feature) => (
-					<div className='flex gap-4' key={feature.title}>
-						<div className='p-2 rounded-lg bg-primary text-primary-foreground h-fit'>
-							<feature.icon className='w-6 h-6' />
+				/>
+				<div className='grid max-w-screen-md grid-cols-1 gap-16 sm:grid-cols-2'>
+					{features.map((feature) => (
+						<div className='flex gap-4' key={feature.title}>
+							<div className='p-2 rounded-lg bg-primary text-primary-foreground h-fit'>
+								<feature.icon className='w-6 h-6' />
+							</div>
+							<div className='flex flex-col items-start'>
+								<span className='text-lg font-semibold'>{feature.title}</span>
+								<span className='text-muted-foreground'>
+									{feature.description}
+								</span>
+							</div>
 						</div>
-						<div className='flex flex-col items-start'>
-							<span className='text-lg font-semibold'>{feature.title}</span>
-							<span className='text-muted-foreground'>
-								{feature.description}
-							</span>
-						</div>
-					</div>
-				))}
+					))}
+				</div>
 			</div>
 		</div>
 	);
