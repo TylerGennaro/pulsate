34d55d59af8af29e9016dc22835b0fa653737f29
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-18 00:14:23 -0400

    adjusted size options

--- src/components/HeroHeading.tsx
+++ src/components/HeroHeading.tsx
@@ -5,7 +5,7 @@ import Balancer from 'react-wrap-balancer';
 interface Props {
 	title: string;
 	description: string;
-	size?: 'sm' | 'md' | 'lg' | 'xl' | 'default';
+	size?: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | 'default';
 	children?: React.ReactNode;
 	className?: string;
 }
@@ -20,20 +20,20 @@ export default function HeroHeading({
 	return (
 		<div
 			className={cn(
-				'flex flex-col items-center gap-2 max-w-screen-sm',
+				'flex flex-col items-center gap-2 max-w-screen-lg',
 				className
 			)}
 		>
 			<Header
 				size={size}
-				className='block leading-tight tracking-tight text-center duration-500 animate-in fade-in slide-in-from-top-6'
+				className='block leading-tight tracking-tight text-center duration-500 animate-in fade-in slide-in-from-bottom-6 bg-gradient-to-br from-black to-zinc-700 dark:from-white dark:to-zinc-400 bg-clip-text !text-transparent'
 			>
 				<Balancer>{title}</Balancer>
 			</Header>
-			<span className='w-3/4 mb-2 leading-normal text-center duration-500 delay-75 text-muted-foreground animate-in fade-in slide-in-from-top-6'>
+			<span className='w-3/4 mb-2 text-lg leading-normal text-center duration-500 delay-75 text-muted-foreground animate-in fade-in slide-in-from-bottom-6'>
 				<Balancer className='text-center'>{description}</Balancer>
 			</span>
-			<div className='flex gap-2 mt-2 duration-500 delay-150 animate-in fade-in slide-in-from-top-6'>
+			<div className='flex gap-2 mt-2 duration-500 delay-150 animate-in fade-in slide-in-from-bottom-6'>
 				{children}
 			</div>
 		</div>
