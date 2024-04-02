370c750a245260f28bd3de2bc4a0b7083b09fd94
Tyler Gennaro <tylergennaro10@gmail.com>
2024-04-01 23:05:04 -0400

    started dashboard stats api route

--- src/app/api/locations/stats/route.ts
+++ src/app/api/locations/stats/route.ts
@@ -1,5 +1,43 @@
+import { authOptions } from '@lib/auth';
+import { db } from '@lib/prisma';
+import { LogType } from '@prisma/client';
+import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
-	return NextResponse.json({}, { status: 200 });
+	const session = await getServerSession(authOptions);
+	if (!session)
+		return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+
+	const locations = await db.location.findMany({
+		select: {
+			name: true,
+			products: {
+				select: {
+					id: true,
+				},
+			},
+		},
+		where: {
+			userId: session.user.id,
+		},
+	});
+	const finalData = await Promise.all([
+		...locations.map(async (location) => {
+			const data = await db.log.aggregate({
+				where: {
+					type: LogType.ITEM_CHECKOUT,
+					productId: {
+						in: location.products.map((product) => product.id),
+					},
+				},
+				_sum: {
+					quantity: true,
+				},
+			});
+			return { name: location.name, data };
+		}),
+	]);
+	console.log(finalData);
+	return NextResponse.json(finalData, { status: 200 });
 }
