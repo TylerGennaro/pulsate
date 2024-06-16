40d518cc24e148abba7535f6f8ce18ee9c9084da
Tyler Gennaro <tylergennaro10@gmail.com>
2024-06-16 00:27:41 -0400

    redesigned

--- src/components/TagBadge.tsx
+++ src/components/TagBadge.tsx
@@ -3,17 +3,22 @@ import { Tag, Tag as TagEnum } from '@lib/enum';
 import { tags } from '@lib/relations';
 import { Badge } from './ui/badge';
 import { VariantProps } from 'class-variance-authority';
+import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 
 export default function TagBadge({ tag }: { tag: TagEnum }) {
 	const tagInfo = tags[tag as Tag];
 	return (
-		<Badge
-			color={tagInfo.color as VariantProps<typeof Badge>['color']}
-			variant='ghost'
-			className='whitespace-nowrap w-fit'
-		>
-			{tagInfo.icon && <tagInfo.icon className='mr-1 w-3 h-3' />}
-			{tagInfo.label}
-		</Badge>
+		<Tooltip>
+			<TooltipTrigger>
+				<Badge
+					color={tagInfo.color as VariantProps<typeof Badge>['color']}
+					variant='ghost'
+					className='p-1 w-fit whitespace-nowrap'
+				>
+					{tagInfo.icon && <tagInfo.icon size={16} />}
+				</Badge>
+			</TooltipTrigger>
+			<TooltipContent>{tagInfo.label}</TooltipContent>
+		</Tooltip>
 	);
 }
