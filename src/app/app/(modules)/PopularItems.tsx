910eae8ed2dc48a50ee353b49f05133f01363c1c
Tyler Gennaro <tylergennaro10@gmail.com>
2024-06-16 00:21:36 -0400

    finished module and logic

--- src/app/app/(modules)/PopularItems.tsx
+++ src/app/app/(modules)/PopularItems.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import BarChart from '@components/BarChart';
 import {
 	Select,
 	SelectContent,
@@ -7,119 +8,41 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@components/ui/select';
-import { Tabs, TabsList, TabsTrigger } from '@components/ui/tabs';
-import { CSSProperties } from 'react';
-import {
-	Bar,
-	BarChart,
-	ResponsiveContainer,
-	Tooltip,
-	XAxis,
-	YAxis,
-} from 'recharts';
+import { useState } from 'react';
+import { DashboardPopularItemData } from '../DashboardModules';
 
-const data = [
-	{
-		name: 'Item 9',
-		quantity: 60,
-	},
-	{
-		name: 'Item 8',
-		quantity: 50,
-	},
-	{
-		name: 'Item 7',
-		quantity: 30,
-	},
-	{
-		name: 'Item 6',
-		quantity: 25,
-	},
-	{
-		name: 'Item 5',
-		quantity: 16,
-	},
-	{
-		name: 'Item 4',
-		quantity: 12,
-	},
-	{
-		name: 'Item 3',
-		quantity: 7,
-	},
-	{
-		name: 'Item 2',
-		quantity: 5,
-	},
-	{
-		name: 'Item 1',
-		quantity: 4,
-	},
-	{
-		name: 'Cervical Collar',
-		quantity: 2,
-	},
-];
+export default function PopularItems({
+	data,
+}: {
+	data: DashboardPopularItemData[];
+}) {
+	const [selectedLocation, setSelectedLocation] = useState<string>(
+		data[0].name
+	);
 
-const CustomTooltip = ({ active, payload, label }: any) => {
-	if (active && payload && payload.length) {
-		return (
-			<div className='px-3 py-2 border rounded-md shadow-md bg-content'>
-				<p>{`${label}: ${payload[0].value}`}</p>
-			</div>
-		);
-	}
-	return null;
-};
-
-export default function PopularItems() {
 	return (
 		<div className='mt-8'>
 			<div className='flex flex-wrap items-center justify-between gap-8'>
-				<Select value='ghdfsm'>
+				<Select value={selectedLocation} onValueChange={setSelectedLocation}>
 					<SelectTrigger className='w-[250px] [&>span]:overflow-hidden [&>span]:overflow-ellipsis [&>span]:whitespace-nowrap [&>span]:pr-1'>
 						<SelectValue />
 					</SelectTrigger>
 					<SelectContent>
-						<SelectItem value='abcded'>Test Location</SelectItem>
-						<SelectItem value='ghdfsm'>
-							Long location name blah blah blah
-						</SelectItem>
+						{data?.map((entry) => (
+							<SelectItem key={entry.name} value={entry.name}>
+								{entry.name}
+							</SelectItem>
+						))}
 					</SelectContent>
 				</Select>
 			</div>
-			<Tabs className='mt-8' defaultValue='week'>
-				<TabsList>
-					<TabsTrigger value='week'>Week</TabsTrigger>
-					<TabsTrigger value='2week'>2 Weeks</TabsTrigger>
-					<TabsTrigger value='month'>Month</TabsTrigger>
-				</TabsList>
-			</Tabs>
-			<div className='h-[200px] mt-8'>
-				<ResponsiveContainer width='100%' height='100%'>
-					<BarChart
-						data={data}
-						margin={{
-							left: -32,
-						}}
-					>
-						<XAxis dataKey='name' />
-						<YAxis />
-						<Tooltip
-							cursor={{ fill: 'hsl(var(--muted))', radius: 4 }}
-							content={<CustomTooltip />}
-							isAnimationActive={false}
-						/>
-						<Bar
-							dataKey='quantity'
-							style={
-								{
-									fill: 'hsl(var(--primary))',
-								} as CSSProperties
-							}
-						/>
-					</BarChart>
-				</ResponsiveContainer>
+			<div className='h-[200px] mt-[106px]'>
+				<BarChart
+					data={
+						data.find((entry) => entry.name === selectedLocation)?.data ?? []
+					}
+					xAxisKey='name'
+				/>
 			</div>
 		</div>
 	);
