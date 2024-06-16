3dbd236be8325d91039e6fecf766112c21b5844d
Tyler Gennaro <tylergennaro10@gmail.com>
2024-06-16 00:26:35 -0400

    moved locations to client and restyled

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -15,12 +15,15 @@ import Indicator, { indicatorVariants } from './ui/indicator';
 import { tags as tagRelations } from '@lib/relations';
 import { Tag } from '@lib/enum';
 import NewLocation from './NewLocation';
+import { ReactNode } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Skeleton } from './ui/skeleton';
 
 function NavBlock({
 	label,
 	children,
 }: {
-	label?: string;
+	label?: string | ReactNode;
 	children: React.ReactNode;
 }) {
 	return (
@@ -61,12 +64,19 @@ function NavButton({
 					className={`${disabled ? 'pointer-events-none' : ''}`}
 				>
 					<Button
-						className={`w-full flex bg-transparent hover:bg-muted text-foreground ${
-							selected ? 'bg-muted' : ''
+						className={`w-full flex bg-transparent hover:bg-muted text-muted-foreground hover:text-foreground group ${
+							selected ? 'bg-muted text-foreground' : ''
 						}`}
 						disabled={disabled}
+						variant='ghost'
 					>
-						{props.icon && <props.icon className='mr-2 shrink-0' />}
+						{props.icon && (
+							<props.icon
+								className={`mr-2 transition-transform shrink-0 group-hover:scale-100 ${
+									selected ? 'scale-100' : 'scale-90'
+								}`}
+							/>
+						)}
 						<div className='w-full overflow-hidden text-left overflow-ellipsis'>
 							<span className='whitespace-nowrap'>{children}</span>
 						</div>
@@ -105,14 +115,19 @@ const navItems = [
 	},
 ];
 
-export function Nav({
-	locations,
-	toggle,
-}: {
-	locations: LocationInfo[] | null;
-	toggle: (open: boolean) => void;
-}) {
+export function Nav({ toggle }: { toggle: (open: boolean) => void }) {
 	const pathname = usePathname();
+	const {
+		data: locations,
+		isLoading,
+	}: { data: LocationInfo[] | undefined; isLoading: boolean } = useQuery({
+		queryKey: ['locations'],
+		queryFn: async () => {
+			const res = await fetch('/api/locations');
+			const json: { locations: LocationInfo[] } = await res.json();
+			return json.locations;
+		},
+	});
 	return (
 		<div className='flex flex-col gap-8'>
 			<NavBlock>
@@ -132,8 +147,15 @@ export function Nav({
 					</NavButton>
 				))}
 			</NavBlock>
-			<NavBlock label='Locations'>
-				{locations &&
+			<NavBlock
+				label={
+					<span className='flex items-center justify-between'>
+						Locations
+						<NewLocation />
+					</span>
+				}
+			>
+				{locations ? (
 					locations.map((location) => (
 						<NavButton
 							key={location.id}
@@ -145,8 +167,14 @@ export function Nav({
 						>
 							{location.name}
 						</NavButton>
-					))}
-				<NewLocation />
+					))
+				) : (
+					<>
+						{Array.from({ length: 3 }).map((_, i) => (
+							<Skeleton key={i} className='h-8 my-1' />
+						))}
+					</>
+				)}
 			</NavBlock>
 		</div>
 	);
