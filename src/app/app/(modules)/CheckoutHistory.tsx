04d16d34dc78855921de1b40e44fe94e5f5bdb48
Tyler Gennaro <tylergennaro10@gmail.com>
2024-06-16 00:21:29 -0400

    finished module logic and styling

--- src/app/app/(modules)/CheckoutHistory.tsx
+++ src/app/app/(modules)/CheckoutHistory.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import BarChart from '@components/BarChart';
 import {
 	Select,
 	SelectContent,
@@ -9,64 +10,44 @@ import {
 } from '@components/ui/select';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/ui/tabs';
 import { useQuery } from '@tanstack/react-query';
-import { CSSProperties, useState } from 'react';
-import {
-	Bar,
-	BarChart,
-	ResponsiveContainer,
-	Tooltip,
-	XAxis,
-	YAxis,
-} from 'recharts';
+import { useState } from 'react';
+import { DashboardDateRangeData } from '../DashboardModules';
 
-type DateRangeEntry = {
-	quantity: number;
-	date: Date;
-};
+function TabChart({
+	data,
+	dataKey,
+	selectedLocation,
+}: {
+	data: DashboardDateRangeData[];
+	dataKey: keyof Omit<DashboardDateRangeData, 'name'>;
+	selectedLocation: string;
+}) {
+	return (
+		<div className='h-[200px] mt-8'>
+			<BarChart
+				data={
+					data.find((entry) => entry.name === selectedLocation)?.[dataKey] ?? []
+				}
+				xAxisKey='date'
+			/>
+		</div>
+	);
+}
 
-type DashboardStat = {
-	name: string;
-	week: DateRangeEntry[];
-	biweek: DateRangeEntry[];
-	month: DateRangeEntry[];
-};
+export default function CheckoutHistory({
+	data,
+}: {
+	data: DashboardDateRangeData[];
+}) {
+	const [selectedLocation, setSelectedLocation] = useState<string>(
+		data[0].name
+	);
 
-const CustomTooltip = ({ active, payload, label }: any) => {
-	if (active && payload && payload.length) {
-		return (
-			<div className='px-3 py-2 border rounded-md shadow-md bg-content'>
-				<p>{`${label} - ${payload[0].value}`}</p>
-			</div>
-		);
-	}
-	return null;
-};
-
-export default function CheckoutHistory() {
-	const [selectedLocation, setSelectedLocation] = useState<string>('');
-	const {
-		data,
-		isPending,
-	}: { data: DashboardStat[] | undefined; isPending: boolean } = useQuery({
-		queryKey: ['checkout-history'],
-		queryFn: async () => {
-			const results = await fetch('/api/locations/stats');
-			const json = await results.json();
-			if (selectedLocation === '') {
-				setSelectedLocation(json[0].name);
-			}
-			return json;
-		},
-	});
-	console.log(data);
 	return (
 		<div className='mt-8'>
 			<div className='flex flex-wrap items-center justify-between gap-8'>
 				<Select value={selectedLocation} onValueChange={setSelectedLocation}>
-					<SelectTrigger
-						isLoading={isPending}
-						className='w-[250px] [&>span]:overflow-hidden [&>span]:overflow-ellipsis [&>span]:whitespace-nowrap [&>span]:pr-1'
-					>
+					<SelectTrigger className='w-[250px] [&>span]:overflow-hidden [&>span]:overflow-ellipsis [&>span]:whitespace-nowrap [&>span]:pr-1'>
 						<SelectValue />
 					</SelectTrigger>
 					<SelectContent>
@@ -85,97 +66,25 @@ export default function CheckoutHistory() {
 					<TabsTrigger value='month'>Month</TabsTrigger>
 				</TabsList>
 				<TabsContent value='week'>
-					<div className='h-[200px] mt-8'>
-						<ResponsiveContainer width='100%' height='100%'>
-							<BarChart
-								data={
-									data?.find((entry) => entry.name === selectedLocation)
-										?.week ?? []
-								}
-								margin={{
-									left: -32,
-								}}
-							>
-								<XAxis dataKey='date' />
-								<YAxis />
-								<Tooltip
-									cursor={{ fill: 'hsl(var(--muted))', radius: 4 }}
-									content={<CustomTooltip />}
-									isAnimationActive={false}
-								/>
-								<Bar
-									dataKey='quantity'
-									style={
-										{
-											fill: 'hsl(var(--primary))',
-										} as CSSProperties
-									}
-								/>
-							</BarChart>
-						</ResponsiveContainer>
-					</div>
+					<TabChart
+						data={data ?? []}
+						dataKey='week'
+						selectedLocation={selectedLocation}
+					/>
 				</TabsContent>
 				<TabsContent value='biweek'>
-					<div className='h-[200px] mt-8'>
-						<ResponsiveContainer width='100%' height='100%'>
-							<BarChart
-								data={
-									data?.find((entry) => entry.name === selectedLocation)
-										?.biweek ?? []
-								}
-								margin={{
-									left: -32,
-								}}
-							>
-								<XAxis dataKey='date' />
-								<YAxis />
-								<Tooltip
-									cursor={{ fill: 'hsl(var(--muted))', radius: 4 }}
-									content={<CustomTooltip />}
-									isAnimationActive={false}
-								/>
-								<Bar
-									dataKey='quantity'
-									style={
-										{
-											fill: 'hsl(var(--primary))',
-										} as CSSProperties
-									}
-								/>
-							</BarChart>
-						</ResponsiveContainer>
-					</div>
+					<TabChart
+						data={data ?? []}
+						dataKey='biweek'
+						selectedLocation={selectedLocation}
+					/>
 				</TabsContent>
 				<TabsContent value='month'>
-					<div className='h-[200px] mt-8'>
-						<ResponsiveContainer width='100%' height='100%'>
-							<BarChart
-								data={
-									data?.find((entry) => entry.name === selectedLocation)
-										?.month ?? []
-								}
-								margin={{
-									left: -32,
-								}}
-							>
-								<XAxis dataKey='date' />
-								<YAxis />
-								<Tooltip
-									cursor={{ fill: 'hsl(var(--muted))', radius: 4 }}
-									content={<CustomTooltip />}
-									isAnimationActive={false}
-								/>
-								<Bar
-									dataKey='quantity'
-									style={
-										{
-											fill: 'hsl(var(--primary))',
-										} as CSSProperties
-									}
-								/>
-							</BarChart>
-						</ResponsiveContainer>
-					</div>
+					<TabChart
+						data={data ?? []}
+						dataKey='month'
+						selectedLocation={selectedLocation}
+					/>
 				</TabsContent>
 			</Tabs>
 		</div>
