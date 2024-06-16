2267d05db889424beb68c90079486d7be3bc6c28
Tyler Gennaro <tylergennaro10@gmail.com>
2024-06-16 00:22:17 -0400

    removed unused code began work on dynamic content

--- src/app/app/(modules)/StockAlerts.tsx
+++ src/app/app/(modules)/StockAlerts.tsx
@@ -1,128 +1,53 @@
 import TagBadge from '@components/TagBadge';
-import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
-} from '@components/ui/accordion';
 import { Tag } from '@lib/enum';
-import { ChevronRight } from 'lucide-react';
 
 const data = [
 	{
-		name: 'Test Location',
-		items: [
-			{
-				name: 'Cervical Collar',
-				quantity: 4,
-				tag: Tag.LOW,
-			},
-			{
-				name: 'Test Item',
-				expires: 'Mar 30, 2024',
-				tag: Tag.EXPIRES,
-			},
-		],
+		name: 'Cervical Collar',
+		location: 'Test Location',
+		quantity: 4,
+		tag: Tag.LOW,
 	},
 	{
-		name: 'Other Location',
-		items: [
-			{
-				name: 'Cervical Collar',
-				quantity: 4,
-				tag: Tag.LOW,
-			},
-		],
+		name: 'Test Item',
+		location: 'Test Location',
+		expires: 'Mar 30, 2024',
+		tag: Tag.EXPIRES,
+	},
+	{
+		name: 'Cervical Collar',
+		location: 'Other Location',
+		quantity: 4,
+		tag: Tag.LOW,
 	},
 ];
 
 export default function StockAlerts() {
 	return (
-		<ul className='mt-8 overflow-hidden rounded-md [&>li:last-of-type_li:last-of-type]:border-none'>
-			{data.map((location) => (
-				<li key={location.name}>
-					<p className='px-4 py-2 border-b bg-background'>{location.name}</p>
-					<ul>
-						{location.items.map((item) => (
-							<li
-								key={item.name}
-								className='flex items-center justify-between p-4 transition-colors border-b hover:bg-muted'
-							>
-								<div>
-									<p>{item.name}</p>
-									<p className='text-sm text-muted-foreground'>
-										{item.quantity
-											? `${item.quantity} items left`
-											: `Expires ${item.expires}`}
-									</p>
-								</div>
-								<div>
-									<TagBadge tag={item.tag} />
-								</div>
-							</li>
-						))}
-					</ul>
+		<ul className='mt-8'>
+			{data.map((item) => (
+				<li
+					key={item.name}
+					className='transition-colors border-b last-of-type:border-none hover:bg-muted'
+				>
+					<a className='flex items-center justify-between p-2'>
+						<div className='flex items-center gap-8'>
+							<div>
+								<TagBadge tag={item.tag} />
+							</div>
+							<div>
+								<p>{item.name}</p>
+								<p className='text-sm text-muted-foreground'>{item.location}</p>
+							</div>
+						</div>
+						<p className='text-muted-foreground'>
+							{item.quantity
+								? `${item.quantity} items left`
+								: `Expires ${item.expires}`}
+						</p>
+					</a>
 				</li>
 			))}
 		</ul>
-		// <Accordion
-		// 	type='single'
-		// 	collapsible
-		// 	className='mt-4 border-t rounded-md border-x bg-background'
-		// >
-		// 	<AccordionItem value='1' className='overflow-hidden rounded-md'>
-		// 		<AccordionTrigger className='px-4 [&[data-state=open]]:border-b bg-content brightness-75'>
-		// 			Test Location
-		// 		</AccordionTrigger>
-		// 		<AccordionContent className='pb-0 bg-content'>
-		// 			<ul>
-		// 				<li className='flex items-center justify-between px-4 py-4 transition-colors cursor-pointer hover:bg-muted'>
-		// 					<span>Cervical Collar</span>
-		// 					<div className='flex items-center gap-8'>
-		// 						<div className='flex gap-2'>
-		// 							<TagBadge tag={Tag.LOW} />
-		// 						</div>
-		// 						<ChevronRight size={16} />
-		// 					</div>
-		// 				</li>
-		// 				<li className='flex items-center justify-between px-4 py-4 transition-colors cursor-pointer hover:bg-muted'>
-		// 					<span>Cervical Collar</span>
-		// 					<div className='flex items-center gap-8'>
-		// 						<div className='flex gap-2'>
-		// 							<TagBadge tag={Tag.LOW} />
-		// 						</div>
-		// 						<ChevronRight size={16} />
-		// 					</div>
-		// 				</li>
-		// 				<li className='flex items-center justify-between px-4 py-4 transition-colors cursor-pointer hover:bg-muted'>
-		// 					<span>Cervical Collar</span>
-		// 					<div className='flex items-center gap-8'>
-		// 						<div className='flex gap-2'>
-		// 							<TagBadge tag={Tag.LOW} />
-		// 							<TagBadge tag={Tag.EXPIRES} />
-		// 						</div>
-		// 						<ChevronRight size={16} />
-		// 					</div>
-		// 				</li>
-		// 			</ul>
-		// 		</AccordionContent>
-		// 	</AccordionItem>
-		// 	<AccordionItem value='2' className='overflow-hidden rounded-md'>
-		// 		<AccordionTrigger className='px-4 bg-content brightness-75 [&[data-state=open]]:border-b'>
-		// 			Other Location
-		// 		</AccordionTrigger>
-		// 		<AccordionContent className='pb-0 bg-content'>
-		// 			<ul>
-		// 				<li className='flex items-center justify-between px-4 py-4 transition-colors cursor-pointer hover:bg-muted'>
-		// 					<span>Cervical Collar</span>
-		// 					<div className='flex items-center gap-8'>
-		// 						<TagBadge tag={Tag.LOW} />
-		// 						<ChevronRight size={16} />
-		// 					</div>
-		// 				</li>
-		// 			</ul>
-		// 		</AccordionContent>
-		// 	</AccordionItem>
-		// </Accordion>
 	);
 }
