f8070d4d7aaa097ed913e9dbbe6b7aae5bcb705d
Tyler Gennaro <tylergennaro10@gmail.com>
2024-06-16 00:25:30 -0400

    moved locations to client

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -5,21 +5,11 @@ import Sidebar from './Sidebar';
 import SiteHeader from './SiteHeader';
 import { useState } from 'react';
 
-export default function AppShell({
-	locations,
-	children,
-}: {
-	locations: LocationInfo[] | null;
-	children: React.ReactNode;
-}) {
+export default function AppShell({ children }: { children: React.ReactNode }) {
 	const [sidebarOpen, setSidebarOpen] = useState(false);
 	return (
 		<div className='flex h-full'>
-			<Sidebar
-				locations={locations}
-				open={sidebarOpen}
-				toggle={setSidebarOpen}
-			/>
+			<Sidebar open={sidebarOpen} toggle={setSidebarOpen} />
 			<div className='flex flex-col w-full h-full shrink'>
 				<SiteHeader sidebarToggle={setSidebarOpen} sidebarOpen={sidebarOpen} />
 				<div className='w-full h-full p-2 mx-auto max-w-screen-2xl lg:p-4 bg-background'>
