0e9214e40e8f83249f9a9f0b68d249333ab6a45b
Tyler Gennaro <tylergennaro10@gmail.com>
2024-06-23 09:42:05 -0400

    restyled location header

--- src/components/location/LocationHeader.tsx
+++ src/components/location/LocationHeader.tsx
@@ -1,8 +1,12 @@
 'use client';
 
 import { ContainerHeader } from '@components/Container';
+import { Button } from '@components/ui/button';
+import Header from '@components/ui/header';
 import { Skeleton } from '@components/ui/skeleton';
 import { useQuery } from '@tanstack/react-query';
+import { ChevronLeft } from 'lucide-react';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 type LocationHeaderProps = {
@@ -30,16 +34,25 @@ export default function LocationHeader({ location }: LocationHeaderProps) {
 	});
 	if (isLoading)
 		return (
-			<div className='flex flex-col gap-2'>
+			<div className='flex flex-col gap-2 my-4'>
 				<Skeleton className='w-40 h-8' />
-				<Skeleton className='w-56 h-6' />
+				<Skeleton className='w-56 h-4' />
 			</div>
 		);
 	if (!data) return notFound();
 	return (
-		<ContainerHeader
-			header={data.name}
-			description={`Managed by ${data.user.name ?? 'Unknown User'}`}
-		/>
+		<div className='flex gap-4 my-4'>
+			<Link href={`/app`} className='block mt-0.5 w-fit'>
+				<Button size='icon' variant='outline' className='bg-content'>
+					<ChevronLeft size={16} />
+				</Button>
+			</Link>
+			<div>
+				<Header size='md'>{data.name}</Header>
+				<span className='text-sm text-muted-foreground'>{`Managed by ${
+					data.user.name ?? 'Unknown User'
+				}`}</span>
+			</div>
+		</div>
 	);
 }
