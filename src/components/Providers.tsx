bc82b51bb09093ea603851f6efd5ad487a285103
Tyler Gennaro <tylergennaro10@gmail.com>
2024-08-06 22:29:23 -0400

    refactor: adjust toaster DOM order

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -24,8 +24,8 @@ export default function Providers({
 					enableSystem
 				>
 					<TooltipProvider delayDuration={0}>
-						<Toaster />
 						<BalancerProvider>{children}</BalancerProvider>
+						<Toaster />
 					</TooltipProvider>
 				</NextThemesProvider>
 			</SessionProvider>
