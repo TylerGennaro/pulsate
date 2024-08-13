57a980da48c22b4e43b52108223cc7a7087190b3
Tyler Gennaro <tylergennaro10@gmail.com>
2024-08-13 18:12:52 -0400

    fix: add auth in action

--- src/actions/locations.ts
+++ src/actions/locations.ts
@@ -1,10 +1,15 @@
 'use server';
 
+import { authOptions } from '@lib/auth';
 import { db } from '@lib/prisma';
 import { ActionResponse, getErrorMessage } from '@lib/utils';
+import { getServerSession } from 'next-auth';
 import { revalidatePath } from 'next/cache';
 
 export async function updateLocationName(locationId: string, newName: string) {
+	const session = await getServerSession(authOptions);
+	if (!session) return ActionResponse.send(false, 'Unauthorized');
+
 	try {
 		await db.location.update({
 			where: {
