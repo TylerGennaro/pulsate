b0b2fe820cad87e47291eb1495a7216778323e6b
Tyler Gennaro <tylergennaro10@gmail.com>
2024-08-15 09:03:41 -0400

    refactor: switch default className to container

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -5,18 +5,18 @@ import { cn } from '@lib/utils';
 export interface InputProps
 	extends React.InputHTMLAttributes<HTMLInputElement> {
 	startIcon?: React.ReactNode;
-	wrapperClass?: string;
+	inputClass?: string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-	({ className, type, wrapperClass, startIcon, ...props }, ref) => {
+	({ className, type, inputClass, startIcon, ...props }, ref) => {
 		return (
-			<div className={cn('relative w-full', wrapperClass)}>
+			<div className={cn('relative w-full', className)}>
 				<input
 					type={type}
 					className={cn(
 						'flex w-full rounded-md bg-muted/30 border outline-none focus-visible:border-muted-foreground/30 px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-300 focus-visible:ring-offset-2 ring-offset-zinc-100 dark:focus-visible:ring-zinc-700 dark:ring-offset-zinc-900',
-						className,
+						inputClass,
 						startIcon ? 'pl-10' : undefined
 					)}
 					ref={ref}
