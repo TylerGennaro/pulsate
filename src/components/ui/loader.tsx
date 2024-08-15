90c4e0fbf28554e3d1912bc5e88b5f02e407b2e4
Tyler Gennaro <tylergennaro10@gmail.com>
2024-08-14 22:47:46 -0400

    refactor: add new loader

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,8 +1,23 @@
+import LoaderSvg from '@components/LoaderSvg';
 import LogoSvg from '@components/LogoSvg';
 import { cn } from '@lib/utils';
 import { cva, VariantProps } from 'class-variance-authority';
 
 const loaderVariants = cva('', {
+	variants: {
+		size: {
+			sm: 'w-4 h-4 [&_path]:stroke-[14]',
+			md: 'w-8 h-8 [&_path]:stroke-[7]',
+			lg: 'w-12 h-12',
+			xl: 'w-16 h-16',
+		},
+	},
+	defaultVariants: {
+		size: 'lg',
+	},
+});
+
+const fancyLoaderVariants = cva('', {
 	variants: {
 		size: {
 			sm: 'w-4 h-4 [&_path]:stroke-[9]',
@@ -18,18 +33,30 @@ const loaderVariants = cva('', {
 
 type LoaderProps = VariantProps<typeof loaderVariants> & {
 	faded?: boolean;
+	fancy?: boolean;
 	className?: string;
 };
 
 export default function Loader({
 	faded,
+	fancy = false,
 	className,
 	size,
 	...props
 }: LoaderProps) {
 	return (
-		<div className={cn(loaderVariants({ size }), className)} {...props}>
-			<LogoSvg animated faded={faded} />
+		<div
+			className={cn(
+				fancy ? fancyLoaderVariants({ size }) : loaderVariants({ size }),
+				className
+			)}
+			{...props}
+		>
+			{fancy ? (
+				<LogoSvg animated faded={faded} {...props} />
+			) : (
+				<LoaderSvg faded={faded} {...props} />
+			)}
 		</div>
 	);
 }
