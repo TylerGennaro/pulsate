f54dbf64d94538f31076b93d70ace4f7bd38bf14
Tyler Gennaro <tylergennaro10@gmail.com>
2024-08-14 22:47:17 -0400

    chore: remove unused import

--- src/app/app/[location]/(inventory)/page.tsx
+++ src/app/app/[location]/(inventory)/page.tsx
@@ -1,4 +1,3 @@
-import ActivityTable from '@components/ActivityTable';
 import InventoryTable from '@components/location/InventoryTable';
 
 export default function Page({ params }: { params: { location: string } }) {
